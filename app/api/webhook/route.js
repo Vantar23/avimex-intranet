c645c5dbb0db63646ddf97cd29642eb4fab64af3
Administrator <david.antonio@controlware.com.mx>
2025-01-06 22:46:43 -0600

    New Run build

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -37,25 +37,28 @@ export async function POST(req) {
         const parsedBody = JSON.parse(body);
         console.log('Webhook recibido:', parsedBody);
 
-        // Ejecutar comando para hacer git pull y reiniciar la aplicación
-        exec('cd C:\\Users\\Administrador\\source\\NEXTJS-DASHBOARD && git pull && pm2 restart nextjs-dashboard', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error al ejecutar git pull o pm2 restart: ${error.message}`);
-                console.error(`Detalles del error: ${stderr}`);
-                return;
+        // Ejecutar los comandos para hacer git pull, build y reiniciar PM2
+        exec(
+            'cd C:\\Users\\Administrador\\source\\NEXTJS-DASHBOARD && git pull && pnpm install && pnpm run build && pm2 restart nextjs-prod',
+            (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error al ejecutar comandos: ${error.message}`);
+                    console.error(`Detalles del error: ${stderr}`);
+                    return;
+                }
+                console.log(`Comando ejecutado exitosamente:\n${stdout}`);
+                console.error(`Errores durante el comando:\n${stderr}`);
             }
-            console.log(`Git Pull Salida: ${stdout}`);
-            console.error(`Errores durante el comando: ${stderr}`);
-        });
+        );
 
-        return NextResponse.json({ message: 'Proyecto actualizado y PM2 reiniciado correctamente' });
+        return NextResponse.json({ message: 'Proyecto actualizado, construido y reiniciado correctamente con PM2' });
     } catch (error) {
         console.error(`Error en webhook: ${error.message}`);
         return NextResponse.json({ message: 'Error procesando el webhook' }, { status: 500 });
     }
 }
 
-// Manejar solicitudes GETTTT
+// Manejar solicitudes GET
 export async function GET() {
     return new NextResponse('<h1>Webhook Server</h1><p>Servidor funcionando correctamente. Ruta POST: /api/webhook.</p>', {
         headers: { 'Content-Type': 'text/html' },
