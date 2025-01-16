e2f6037a12a01f9eb72fa9cc2a232cb2b899e450
David A. Arenas <davidantarenas@icloud.com>
2025-01-15 22:35:51 -0600

    Test

--- app/api/catalogos.js
+++ app/api/catalogos.js
@@ -10,16 +10,23 @@ let catalogos = {
 // Función para obtener los catálogos y separarlos
 const fetchCatalogos = async () => {
     try {
+        console.log('Iniciando solicitud para obtener catálogos...');
         const response = await axios.get('http://localhost/backend/api/Catalogos');
 
-        // Asigna cada sección del JSON a su respectivo array
-        catalogos.Cat_Producto = response.data.Cat_Producto || [];
-        catalogos.Cat_Medida = response.data.Cat_Medida || [];
-        catalogos.Cat_Marca = response.data.Cat_Marca || [];
-        catalogos.Cat_Proveedor = response.data.Cat_Proveedor || [];
+        if (response && response.data) {
+            console.log('Respuesta de la API recibida:', response.data);
 
-        console.log('Datos de los catálogos actualizados.');
-        return catalogos;
+            // Asigna cada sección del JSON a su respectivo array
+            catalogos.Cat_Producto = response.data.Cat_Producto || [];
+            catalogos.Cat_Medida = response.data.Cat_Medida || [];
+            catalogos.Cat_Marca = response.data.Cat_Marca || [];
+            catalogos.Cat_Proveedor = response.data.Cat_Proveedor || [];
+
+            console.log('Datos de los catálogos actualizados:', catalogos);
+            return catalogos;
+        } else {
+            throw new Error('La respuesta de la API está vacía o malformada.');
+        }
     } catch (error) {
         console.error('Error al obtener los datos del catálogo:', error.message);
         throw error; // Lanza el error para que pueda ser manejado en otro lugar si es necesario
