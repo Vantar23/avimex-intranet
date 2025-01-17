20c27e9919a648f3cfa0286e8a214f784411d6dd
David A. Arenas <davidantarenas@icloud.com>
2025-01-17 10:58:48 -0600

    s

--- app/dashboard/form/page.tsx
+++ app/dashboard/form/page.tsx
@@ -127,12 +127,12 @@ export default function Requisicion() {
               className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
             >
               <option value="">Seleccionar...</option>
-              {productos.map((producto) => (
-                <option key={producto.ID} value={producto.ID}>
-                  {producto.DESCRIPCION}
-                </option>
-              ))}
+              <option value="1">Producto 1</option>
+              <option value="2">Producto 2</option>
+              <option value="3">Producto 3</option>
+              <option value="4">Producto 4</option>
             </select>
+
           </div>
         </div>
 
