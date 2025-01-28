911b6c72ca7fc27e1c3bb053b79a6a8df00b244d
David Arenas <davidantarenas@icloud.com>
2025-01-28 10:45:21 -0600

    Cambios al formulario

--- app/dashboard/form/page.tsx
+++ app/dashboard/form/page.tsx
@@ -130,8 +130,8 @@ export default function FormularioCompleto() {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-gray-100 rounded shadow-md">
-      <h1 className="text-2xl font-bold text-gray-800 mb-6">Formulario de Compras</h1>
+    <div className="max-w-4xl mx-auto p-6 rounded ">
+      <h1 className="text-2xl font-bold text-gray-800 mb-6">Requisición</h1>
       <form
         onSubmit={manejarEnvio}
         className="grid grid-cols-1 md:grid-cols-2 gap-4"
