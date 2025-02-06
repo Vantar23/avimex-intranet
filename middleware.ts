023a64debaf684bdc2bfc5378127583ad3e39bc9
David Arenas <davidantarenas@icloud.com>
2025-02-05 19:43:57 -0600

    Corrección de acceso a middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,12 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("session")?.value;
 
-  // Definir rutas protegidas
   const protectedRoutes = ["/dashboard", "/perfil", "/configuracion"];
   const isProtectedRoute = protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route));
 
   if (isProtectedRoute && !token) {
-    // Redirigir al login si no hay token
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
