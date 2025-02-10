a29884b42cdceb8ee19cdc5ac635c65ed3a46f79
Administrator <david.antonio@controlware.com.mx>
2025-02-10 11:36:36 -0600

    Web y next config actualizados

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // Deja el resto de tu configuración aquí si tienes otras opciones
+  redirects: async () => [
+    {
+      source: "/:path*",
+      has: [{ type: "host", value: "avimexintranet.com" }],
+      destination: "https://avimexintranet.com:3000/:path*",
+      permanent: true,
+    },
+  ],
 };
+  
 
 export default nextConfig;
