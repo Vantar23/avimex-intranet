0b2830d4a4a6f0d1591dcd7ec8b8f4bf59541517
Administrator <david.antonio@controlware.com.mx>
2025-02-12 16:05:28 -0600

    Uso de Http y https DNS habilitado

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  redirects: async () => [
-    {
-      source: "/:path*",
-      has: [{ type: "host", value: "avimexintranet.com" }],
-      destination: "https://avimexintranet.com:3000/:path*",
-      permanent: true,
-    },
-  ],
+
 };
   
 
