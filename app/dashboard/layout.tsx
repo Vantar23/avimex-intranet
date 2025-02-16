0b1ba29c7a403eb30193c6047b7402c054699d73
David Arenas <davidantarenas@icloud.com>
2025-02-15 18:07:10 -0600

    Cambios al sidenav

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,15 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
- 
+import SideNav from "@/app/ui/dashboard/sidenav";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
+    <div className="relative h-screen">
+      {/* SideNav flotante */}
+      <SideNav />
+
+      {/* Contenido principal ocupa toda la pantalla */}
+      <div className="h-full w-full p-6 md:overflow-y-auto md:p-12">
+        {children}
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
 }
