3407e418b9ae897c6272bdca5810918b95f81d05
David Arenas <davidantarenas@icloud.com>
2025-02-18 06:46:03 -0600

    Elimina Container del grid

--- app/dashboard/grid/page.tsx
+++ app/dashboard/grid/page.tsx
@@ -311,7 +311,7 @@ export default function Page() {
   };
 
   return (
-    <div className="container mx-auto p-6">
+    <div className=" mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6 text-center">Grid de Requisiciones</h1>
       {error && <p className="text-red-500">{error}</p>}
       {loading ? (
