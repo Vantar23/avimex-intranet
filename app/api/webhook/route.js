51c193766ee780b05d24e2bfaba6c453c61c63b2
server <davidantarenas@icloud.com>
2025-03-07 14:18:30 -0600

    1

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { exec } from 'child_process';
 
-const SECRET = 'SuperSecretPassword'; // Cambia esto por tu secreto configurado en GitHub
+const SECRET = '1234'; // Cambia esto por tu secreto configurado en GitHub
 
 async function verifySecret(req) {
   const signature = req.headers.get('x-hub-signature-256');
