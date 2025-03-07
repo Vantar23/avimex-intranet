35be344cd4cfd9979420729815ebe942f914a304
David Arenas <davidantarenas@icloud.com>
2025-03-07 14:51:20 -0600

    test

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -78,4 +78,4 @@ export async function GET() {
     '<h1>Webhook Server</h1><p>Servidor funcionando correctamente. Ruta POST: /api/webhook.</p>',
     { headers: { 'Content-Type': 'text/html' } }
   );
-}
+}//TEST
