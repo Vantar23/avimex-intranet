6bce6c3454152830fb13b0fe8196021d35d4b7a4
David Arenas <davidantarenas@icloud.com>
2025-03-10 18:48:01 -0600

    a

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div className="min-h-screen bg-white">
       <section className="mb-10 mt-10 text-center">
-          <h2 className="text-3xl font-semibold text-gray-800">Bienvenido a la demo de la Nueva Intranet de Avimex</h2>
+          <h2 className="text-3xl font-semibold text-gray-800">Bienvenido a la demo de la Nuevaaaa Intranet de Avimex</h2>
       </section>
     </div>
   );
